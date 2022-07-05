195f93d7524e68767eb0b6eb67db2b458e951007
Cyber ALAMiN <100506616+Cyber-ALAMiN@users.noreply.github.com>
2022-07-05 11:19:03 +0600

    Create ALAMiN.py

---
+++ ALAMiN.py
@@ -0,0 +1,14 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    os.system('git pull')
+    import requests
+    
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools/nOpened By Cyber ALAMiN")
+    print(' Join Over Facebook Group For Any Help 😍 ')
+    os.system('xdg-open https://www.facebook.com/groups/923481061680273/?ref=share');time.sleep(3)
+    from DDOS import DDOS
+    DDOS()
+    main()
