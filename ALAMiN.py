fb0523146bfdfd0683c82845eff21089873432e5
Cyber ALAMiN <100506616+Cyber-ALAMiN@users.noreply.github.com>
2022-07-06 02:24:08 +0600

    Update ALAMiN.py

--- ALAMiN.py
+++ ALAMiN.py
@@ -1,12 +1,22 @@
-import os, platform
-os.system('git pull')
- 
-import requests
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    os.system('git pull')
+    import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools/nTool Make By Cyber ALAMiN")
+    print(' Join Over Facebook Group For Any Help 😍 ')
+    os.system('xdg-open https://www.facebook.com/groups/923481061680273/?ref=share');time.sleep(3)
     from DDOS import menu
     menu()
 elif bit == '32bit':
+    print("\x1b[1;92m Congratulations ! Your Device Support this Tools/nTool Make By Cyber ALAMiN")
+    print(' Join Over Facebook Group For Any Help 😍 ')
+    os.system('xdg-open https://www.facebook.com/groups/923481061680273/?ref=share');time.sleep(3)
     from DDOS import menu
     menu()
+
  
