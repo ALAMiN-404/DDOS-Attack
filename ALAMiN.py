3d4576a5c98e33ab4b78069d2efc54431a5845ac
Cyber ALAMiN <100506616+Cyber-ALAMiN@users.noreply.github.com>
2022-07-05 11:56:34 +0600

    Update ALAMiN.py

--- ALAMiN.py
+++ ALAMiN.py
@@ -1,14 +1,18 @@
-import os, platform, time
+import os, platform
 try:
     import requests
 except:
     os.system('pip install requests')
-    os.system('git pull')
-    import requests
-    
-    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools/nOpened By Cyber ALAMiN")
-    print(' Join Over Facebook Group For Any Help 😍 ')
-    os.system('xdg-open https://www.facebook.com/groups/923481061680273/?ref=share');time.sleep(3)
-    from DDOS import DDOS
-    DDOS()
-    main()
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
+    os.system('xdg-open https://facebook.com/groups/1017905562448002/')
+if __name__ == "__main__":
+	try:
+		__import__("DDOS").login()
+	except Exception as e:
+		exit(str(e))
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
