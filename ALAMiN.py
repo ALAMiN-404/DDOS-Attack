774062e067662643b1c8f856d6d0e4eae6685d54
Cyber ALAMiN <100506616+Cyber-ALAMiN@users.noreply.github.com>
2022-07-06 02:29:33 +0600

    Update ALAMiN.py

--- ALAMiN.py
+++ ALAMiN.py
@@ -7,15 +7,17 @@ except:
     import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools/nTool Make By Cyber ALAMiN")
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+    print("\n\x1b[1;92m Tool Make By Cyber ALAMiN")
     print(' Join Over Facebook Group For Any Help 😍 ')
-    os.system('xdg-open https://www.facebook.com/groups/923481061680273/?ref=share');time.sleep(3)
+    os.system('xdg-open https://www.facebook.com/groups/923481061680273/?ref=share');time.sleep(5)
     from DDOS import menu
     menu()
 elif bit == '32bit':
-    print("\x1b[1;92m Congratulations ! Your Device Support this Tools/nTool Make By Cyber ALAMiN")
+    print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+    print("\n\x1b[1;92m Tool Make By Cyber ALAMiN")
     print(' Join Over Facebook Group For Any Help 😍 ')
-    os.system('xdg-open https://www.facebook.com/groups/923481061680273/?ref=share');time.sleep(3)
+    os.system('xdg-open https://www.facebook.com/groups/923481061680273/?ref=share');time.sleep(5)
     from DDOS import menu
     menu()
 
